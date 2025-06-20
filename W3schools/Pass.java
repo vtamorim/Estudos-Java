c80b0b0b39d146e887789d5d417cf801a164a97e
Victor Miguel <vthero89@gmail.com>
2025-06-20 12:21:08 +0000

    Gerador de Senha Feito

--- W3schools/Pass.java
+++ W3schools/Pass.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 import java.util.Random;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
+
 public class Pass {
-    static String list_charac = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890;!@#$%¨&*()-=+^`^][";
     public static void main(String[] args) {
+        String list_charac = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890;!@#$%¨&*()-=+^`^][";
         Scanner entry = new Scanner(System.in);
         Random aleatorio = new Random();
         ArrayList<String> senha = new ArrayList<String>();
@@ -13,9 +14,16 @@ public class Pass {
         System.out.print("Tamanho de sua senha: ");
         int range = entry.nextInt();
         
+        String[] listinha = list_charac.split("(?!^)");
         for (int i = 0; i <= range; i++) {
-            
+            int valor = aleatorio.nextInt(list_charac.length())+1;
+            senha.add(listinha[valor]);
         }
+
+        String juntador = String.join("", senha);
+        
+        System.out.println("Sua Senha: " + juntador);
+
         entry.close();
     }
 }
