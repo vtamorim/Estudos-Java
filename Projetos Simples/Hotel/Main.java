14546da869a9a96bf0a2ff3f9b8b3aa1043ab862
Victor Miguel <vthero89@gmail.com>
2025-08-02 19:26:53 -0300

    Aprendendo a Leitura de JSON

--- Projetos Simples/Hotel/Main.java
+++ Projetos Simples/Hotel/Main.java
@@ -1,6 +1,7 @@
 package Hotel;
 import java.util.*;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -10,31 +11,16 @@ class Main{
     static Date data = new Date();
     static List<String> listinha = new ArrayList<>();
     public static void main(String[] args){
-        for (int i = 100; i < 400; i++) {
-            String quarto = Integer.toString(i);
-            if (Integer.parseInt(quarto.substring(1,3))< 11) {
-                listinha.add(quarto);
-            }
-        }
-
-            try(FileWriter Quartos_disp = new FileWriter("Hotel/disponiveis.json");){
-                Quartos_disp.write("[\n");
-                for (int i = 0; i < listinha.size(); i++) {
-                    String e = listinha.get(i);
-                    Quartos_disp.write("{\"Quarto\"" + ":\"" + e + "\"}");
-                     if (i < listinha.size() - 1) {
-                    Quartos_disp.write(",\n");
-            }
-                }
-                Quartos_disp.write("\n]");
-                Quartos_disp.close();
-            }
-            catch(IOException que){
-                System.out.println("deu ruim");
-                que.printStackTrace();
-            }
-
-        
+        File myObj = new File("Hotel/disponiveis.json");
+    if (myObj.exists()) {
+      System.out.println("File name: " + myObj.getName());
+      System.out.println("Absolute path: " + myObj.getAbsolutePath());
+      System.out.println("Writeable: " + myObj.canWrite());
+      System.out.println("Readable " + myObj.canRead());
+      System.out.println("File size in bytes " + myObj.length());
+    } else {
+      System.out.println("The file does not exist.");
+    }
 
     }
 }
