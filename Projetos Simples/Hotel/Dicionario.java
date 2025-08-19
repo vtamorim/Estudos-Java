73a2d1a4d75ef71b9c5d530bf6da16f13d702882
Victor Miguel <vthero89@gmail.com>
2025-08-19 18:36:08 -0300

    Melhorando o código

--- Projetos Simples/Hotel/Dicionario.java
+++ Projetos Simples/Hotel/Dicionario.java
@@ -1,11 +1,14 @@
 package Hotel;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+
+
 
 public class Dicionario {
     public static void main(String[] args) {
         Map<String, String> dicionario = new HashMap<>();
-
+        Scanner Entrada = new Scanner(System.in);
 
 
         for (int i = 100; i < 400; i++) {
@@ -18,9 +21,24 @@ public class Dicionario {
 
 
 
-        // Percorrer o dicionário
-        for (String chave : dicionario.keySet()) {
+        
+
+        
+        while (true) {
+            System.out.println("Reserva de Hotel '-' ");
+            for (String chave : dicionario.keySet()) {
             System.out.println(chave + " -> " + dicionario.get(chave));
-        }
+        }   
+        String Escolha = Entrada.nextLine();
+        for (String verificador : dicionario.keySet()) {
+            if (verificador == Escolha) {
+                System.out.println("Deu certo");
+            }
+        } 
+        Entrada.close();
     }
+
+       
+    }
+
 }
