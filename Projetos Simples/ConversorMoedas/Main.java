20319ad301c0ce228fe8e0834e00d4d5e1ad416c
Victor Miguel <vthero89@gmail.com>
2025-08-25 21:02:00 -0300

    Finalizando o Conteúdo
    Falta Estilizar e Conectar com uma API

--- Projetos Simples/ConversorMoedas/Main.java
+++ Projetos Simples/ConversorMoedas/Main.java
@@ -18,35 +18,61 @@ public class Main {
 
 
         JComboBox<String> select = new JComboBox<>(opcoes);
-
+        JComboBox<String> sele_final = new JComboBox<>(opcoes);
         select.addActionListener(e -> {
             String selecionado = (String) select.getSelectedItem();
             System.out.println(selecionado);
         });
 
-        JPanel titulo = new JPanel();
-        JPanel Inicial  = new JPanel();
+        JPanel titulo = new JPanel(new GridLayout(2, 1));
+        titulo.setPreferredSize(new Dimension(300, 50));
+        titulo.setMaximumSize(new Dimension(300, 50));
+        JPanel Inicial  = new JPanel(new GridLayout(2, 1));
+        Inicial.setPreferredSize(new Dimension(300, 50));
+        Inicial.setMaximumSize(new Dimension(300, 50));
+        JPanel Conversao = new JPanel(new GridLayout(2, 1));
+        Conversao.setPreferredSize(new Dimension(300, 50));
+        Conversao.setMaximumSize(new Dimension(300, 50));
+
 
         JLabel texto_tit = new JLabel("Conversor de Moedas");
         JLabel de = new JLabel("De: ");
-
-
+        de.setAlignmentX(Component.CENTER_ALIGNMENT);
+        TextField valor_inicial = new TextField(15);
         
+        TextField valor_final = new TextField(15);
+
+
         
         texto_tit.setFont(new Font("Arial",Font.BOLD,32));
         
 
 
-    
+        titulo.setBackground(new Color(0,0,0));
+        Inicial.setBackground(new Color(0,0,0));
+        Conversao.setBackground(new Color(0,0,0));
+        
 
+
+
+
+
+        Button submit = new Button("Enviar");
+    
+        
         titulo.add(texto_tit);        
         Inicial.add(de);
+        Inicial.add(valor_inicial);
+        Inicial.add(select);
+        Conversao.add(sele_final);
+        Conversao.add(valor_final);
 
         janela.add(titulo);
         janela.add(Inicial);
-        janela.add(select);
+        janela.add(Conversao);
+        janela.add(submit);
 
-
+        
 
         janela.setVisible(true);
     }
