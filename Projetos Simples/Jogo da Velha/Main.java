148a9a45d92bfd6ba9b91a8923c2f68b2840bfda
Victor Miguel <vthero89@gmail.com>
2025-09-22 21:04:19 +0000

    acho que vou explodir

---
+++ Projetos Simples/Jogo da Velha/Main.java
@@ -0,0 +1,16 @@
+
+
+class Main{
+
+    public static void main(String[] args) {
+        
+        int[][] matriz = new int[3][3];
+        System.out.println("djabeisso");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + " ");
+                System.out.println();
+            }
+        }
+    }
+}
